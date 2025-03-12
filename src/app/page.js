a5785d65b69a26d3bdd4c042e62a08b6d32ecb2d
Jhonny Alex <112901449+jhonnyalexr@users.noreply.github.com>
2025-03-12 09:37:12 -0300

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,52 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+ import { UnicoSDK } from 'idpay-b2b-sdk/index.js'
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  const token = ""; // Inserir o token do response do processo
+  const processId = ""; // Inserir o ProcessID do processo
+
+  const UnicoSDK = window.UnicoSDK.UnicoSDK;
+
+  const Init = () => {
+    UnicoSDK.init({
+      env: 'uat', // Só irá ser preenchido se for ambiente de testes.
+      token,
+    });
+  };
+
+  const Open = () => {
+    UnicoSDK.open({
+      transactionId: processId,
+      token: token,
+      onFinish: onFinishCallback
+    });
+  };
+
+  const onFinishCallback = process => {
+    console.log('Process', process);
+  }
+
+  const Close = () => {
+
+    UnicoSDK.close();
+
+  };
+
+
+  return (
+    <main className="container">
+      <img src="/next.svg" alt="Next.js Logo" className="logo" />
+      <h1>Unico POC CBU</h1>
+      <div className="botoes">
+        <button onClick={Init}>Init</button>
+        <button onClick={Open}>Open</button>
+        <button onClick={Close}>Close</button>
+      </div>
+      <div id="master-div" className="iframe-container">
+      <div id="unico_iframe_embedded"></div>
+     </div>
+
+    </main>
   );
 }
